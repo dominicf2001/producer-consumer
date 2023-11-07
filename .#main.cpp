f4b47f515bff4eba4b9b135f5c68137bed90d436
dominic <dominicf2001@gmail.com>
2023-11-07 14:35:36 -0500

    Add semaphore initialization

--- .#main.cpp
+++
@@ -1,1 +0,0 @@
-dominic@dominic-Lenovo-IdeaPad-FLEX-15IWL.45546:1698774042
--- producer.c
+++ producer.c
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <sys/mman.h>
+#include <semaphore.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
 
 struct shared_memory {
-    int** resources
+    int** resources;
+    sem_t sem;
 };
 
 int main(int argc, char** argv){
@@ -19,8 +21,7 @@ int main(int argc, char** argv){
     }
 
     // set the memory size of the shared memory object to store the struct
-    int truncate_status = ftruncate(file_descriptor, sizeof(struct shared_memory))
-    if (truncate_status == -1) {
+    if (ftruncate(file_descriptor, sizeof(struct shared_memory)) == -1) {
         std::cerr << "Error on ftruncate\n";
         return -1;
     }
@@ -32,6 +33,10 @@ int main(int argc, char** argv){
         return -1;
     }
 
+    if (sem_init(shmp->sem, 1, 0) == -1) {
+        std::cerr << "Error on sem_init\n";
+        return -1;        
+    }
 
     // unlink the shared memory object
     shm_unlink(sm_path);
