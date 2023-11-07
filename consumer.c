d8d7875dff5d6b5e4ca376c4d7fb08a2051709ea
dominic <dominicf2001@gmail.com>
2023-11-07 15:02:11 -0500

    fixes and resource array allocation

--- consumer.c
+++ consumer.c
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <sys/mman.h>
+#include <semaphore.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
-
+#include "./shm.h"
 
 int main(int argc, char** argv){
+    const char* sm_path = "/test";
     
+    // open the shared memory object
+    int oflag = O_RDWR | O_CREAT | O_EXCL;
+    int file_descriptor = shm_open(sm_path, oflag, S_IRWXU);
+    if (file_descriptor == -1) {
+        std::cerr << "Error on shm_open\n";
+        return -1;
+    }
+
+    // set the memory size of the shared memory object to store the struct
+    if (ftruncate(file_descriptor, sizeof(struct shared_memory)) == -1) {
+        std::cerr << "Error on ftruncate\n";
+        return -1;
+    }
+
+    // map the shared memory object into this processes' memory space
+    struct shared_memory* shmp = mmap(NULL, sizeof(struct shared_memory), PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
+    if (shmp == MAP_FAILED) {
+        std::cerr << "Error on mmap\n";
+        return -1;
+    }
+
+    // wait for the producer to produce
+    if (sem_wait(&shmp->sem) == -1) {
+        std::cerr << "Error on sem_init\n";
+        return -1;
+    }
+
+    // unlink the shared memory object
+    shm_unlink(sm_path);
 }
--- producer.c
+++ producer.c
@@ -1,42 +1,47 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <semaphore.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
-
-struct shared_memory {
-    int** resources;
-    sem_t sem;
-};
+#include "./shm.h"
 
 int main(int argc, char** argv){
     const char* sm_path = "/test";
     
-    // create the shared memory object
+    // open the shared memory object
     int oflag = O_RDWR | O_CREAT | O_EXCL;
     int file_descriptor = shm_open(sm_path, oflag, S_IRWXU);
     if (file_descriptor == -1) {
-        std::cerr << "Error on shm_open\n";
+        printf("Error on shm_open\n");
         return -1;
     }
 
     // set the memory size of the shared memory object to store the struct
     if (ftruncate(file_descriptor, sizeof(struct shared_memory)) == -1) {
-        std::cerr << "Error on ftruncate\n";
+        printf("Error on ftruncate\n");
         return -1;
     }
 
     // map the shared memory object into this processes' memory space
     struct shared_memory* shmp = mmap(NULL, sizeof(struct shared_memory), PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
     if (shmp == MAP_FAILED) {
-        std::cerr << "Error on mmap\n";
+        printf("Error on mmap\n");
         return -1;
     }
 
-    if (sem_init(shmp->sem, 1, 0) == -1) {
-        std::cerr << "Error on sem_init\n";
-        return -1;        
+    // initialize the semaphore
+    if (sem_init(&shmp->sem, 1, 0) == -1) {
+        printf("Error on sem_init\n");
+        return -1;
     }
+
+    // allocate memory for the resources array
+    shmp->resources = malloc(BUFFER_SIZE * sizeof(int));
+
+    // set a test value
+    shmp->resources[0] = 2;
 
     // unlink the shared memory object
     shm_unlink(sm_path);
